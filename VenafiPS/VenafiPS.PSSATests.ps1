68992d761dfa2fcb531de9f20f8a194130fad859
Greg Brownstein <greg@jagtechnical.com>
2021-05-05 00:45:04 -0400

    build

--- VenafiPS/VenafiPS.PSSATests.ps1
+++ VenafiPS/VenafiPS.PSSATests.ps1
@@ -1,4 +1,4 @@
-$here = $PSScriptRoot
+$here = 'C:\data\code\VenafiPS\VenafiPS'
 Write-Output "module path: $here"
 # $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
@@ -28,21 +28,38 @@ if (Test-Path -Path "$modulePath\Public\*.ps1") {
     $functionPaths += Get-ChildItem -Path "$modulePath\Public\*.ps1" -Exclude "*.Tests.*"
 }
 
-# Running the analysis for each function
-foreach ($functionPath in $functionPaths) {
-    $functionName = $functionPath.BaseName
+$TestCases = @()
+$functionPaths.ForEach{$TestCases += @{FunctionPath = $_}}
+Describe "Function Analysis with PSScriptAnalyzer" {
+    Context 'Standard Rules' {
+        # Define PSScriptAnalyzer rules
+        $scriptAnalyzerRules = Get-ScriptAnalyzerRule # Just getting all default rules
 
-    Describe "'$functionName' Function Analysis with PSScriptAnalyzer" {
-        Context 'Standard Rules' {
-            # Define PSScriptAnalyzer rules
-            $scriptAnalyzerRules = Get-ScriptAnalyzerRule # Just getting all default rules
-
-            # Perform analysis against each rule
-            forEach ($rule in $scriptAnalyzerRules) {
-                It "should pass '$rule' rule" {
-                    Invoke-ScriptAnalyzer -Path $functionPath -IncludeRule $rule | Should -BeNullOrEmpty
-                }
+        # Perform analysis against each rule
+        forEach ($rule in $scriptAnalyzerRules) {
+            It "should pass '$rule' rule" -TestCases $TestCases {
+                Param($FunctionPath)
+                Invoke-ScriptAnalyzer -Path $FunctionPath -IncludeRule $rule | Should -BeNullOrEmpty
             }
         }
     }
 }
+
+# Running the analysis for each function
+# foreach ($functionPath in $functionPaths) {
+#     $functionName = $functionPath.BaseName
+
+#     Describe "'$functionName' Function Analysis with PSScriptAnalyzer" {
+#         Context 'Standard Rules' {
+#             # Define PSScriptAnalyzer rules
+#             $scriptAnalyzerRules = Get-ScriptAnalyzerRule # Just getting all default rules
+
+#             # Perform analysis against each rule
+#             forEach ($rule in $scriptAnalyzerRules) {
+#                 It "should pass '$rule' rule" {
+#                     Invoke-ScriptAnalyzer -Path $functionPath -IncludeRule $rule | Should -BeNullOrEmpty
+#                 }
+#             }
+#         }
+#     }
+# }
