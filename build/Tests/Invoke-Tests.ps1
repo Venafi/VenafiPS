bea973ae2edfea39c55aa4a42e8f236f514ce253
Greg Brownstein <greg@jagtechnical.com>
2021-05-04 21:58:21 -0400

    Create Invoke-Tests.ps1

---
+++ build/Tests/Invoke-Tests.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = 'stop'
+
+Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
+Install-Module -Name Pester -Scope CurrentUser -Force -Confirm:$false -SkipPublisherCheck -MaximumVersion 4.999.999
+Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Confirm:$false
+Import-Module -Name Pester, PSScriptAnalyzer
+
+$failure = $false
+
+$tests = @{
+    OutputFile   = 'TestResults.xml'
+    OutputFormat = 'NUnitXml'
+    Script       = '.\Tests'
+    PassThru     = $true
+}
+$results = Invoke-Pester @tests
+if ( $results.FailedCount -gt 0 ) {
+    $failure = $true
+}
+
+$tests.OutputFile = 'PSSAResults.xml'
+$tests.Script = "$PSScriptRoot\CommonPSSA.tests.ps1"
+$results = Invoke-Pester @tests
+if ( $results.FailedCount -gt 0 ) {
+    $failure = $true
+}
+
+if ( $failure ) {
+    throw "at least one test failed"
+}
