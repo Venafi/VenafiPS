b6e9e88f9c6df0c56573bea3939c62ad99e29d23
Greg Brownstein <greg@jagtechnical.com>
2021-06-14 18:50:34 -0400

    get cert count

---
+++ VenafiPS/Private/New-HttpQueryString.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    Generate http query string
+.LINK
+    https://powershellmagazine.com/2019/06/14/pstip-a-better-way-to-generate-http-query-strings-in-powershell/
+#>
+function New-HttpQueryString
+{
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Mandatory = $true)]
+        [String]
+        $Uri,
+
+        [Parameter(Mandatory = $true)]
+        [Hashtable]
+        $QueryParameter
+    )
+    # Add System.Web
+    Add-Type -AssemblyName System.Web
+    
+    # Create a http name value collection from an empty string
+    $nvCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
+    
+    foreach ($key in $QueryParameter.Keys)
+    {
+        $nvCollection.Add($key, $QueryParameter.$key)
+    }
+    
+    # Build the uri
+    $uriRequest = [System.UriBuilder]$uri
+    $uriRequest.Query = $nvCollection.ToString()
+    
+    return $uriRequest.Uri.OriginalString
+}
