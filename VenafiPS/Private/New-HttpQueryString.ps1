f5270ffddb2b0452a42bf206c4396f82ca3da2d5
Greg Brownstein <greg@jagtechnical.com>
2021-07-21 17:29:18 -0400

    -Force parameter for Revoke functions (#18)
    * -force for revoke functions
    
    * changelog update
    
    * small help update
    
    * help update
    
    * pssa fixes
    
    * wakey wakey GitHub Actions
    
    * help update
    
    * pssa fix

--- VenafiPS/Private/New-HttpQueryString.ps1
+++ VenafiPS/Private/New-HttpQueryString.ps1
@@ -4,10 +4,11 @@
 .LINK
     https://powershellmagazine.com/2019/06/14/pstip-a-better-way-to-generate-http-query-strings-in-powershell/
 #>
-function New-HttpQueryString
-{
+function New-HttpQueryString {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseShouldProcessForStateChangingFunctions', '', Justification = 'No state is actually changing')]
+
     [CmdletBinding()]
-    param 
+    param
     (
         [Parameter(Mandatory = $true)]
         [String]
@@ -19,18 +20,17 @@ function New-HttpQueryString
     )
     # Add System.Web
     Add-Type -AssemblyName System.Web
-    
+
     # Create a http name value collection from an empty string
     $nvCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
-    
-    foreach ($key in $QueryParameter.Keys)
-    {
+
+    foreach ($key in $QueryParameter.Keys) {
         $nvCollection.Add($key, $QueryParameter.$key)
     }
-    
+
     # Build the uri
     $uriRequest = [System.UriBuilder]$uri
     $uriRequest.Query = $nvCollection.ToString()
-    
+
     return $uriRequest.Uri.OriginalString
 }
