52d8eade96f90178b2abe11b35eba1ed7cf61490
Chris Lyttle <chris.lyttle@venafi.com>
2022-01-13 14:03:24 -0700

    Changes based on PR review

--- VenafiPS/Public/New-TppCustomField.ps1
+++ VenafiPS/Public/New-TppCustomField.ps1
@@ -45,17 +45,17 @@ Query returns a PSCustomObject with the following properties:
     Type
 
 .EXAMPLE
-New-VenafiCustomField -Name "Last Date" -Label "Last Date" -Class "X509 Certificate" -Type "1" -RenderReadOnly -Help "Last Date of certificate import"
+New-TppCustomField -Name "Last Date" -Label "Last Date" -Class "X509 Certificate" -Type "1" -RenderReadOnly -Help "Last Date of certificate import"
 Create new custom field for certificates
 
 .LINK
 http://VenafiPS.readthedocs.io/en/latest/functions/New-TppCustomField/
 
 .LINK
-https://docs.venafi.com/Docs/20.4SDK/TopNav/Content/SDK/WebSDK/r-SDK-POST-Metadata-DefineItem.php?tocpath=Web%20SDK%7CMetadata%20custom%20fields%20programming%20interface%7C_____4
+https://docs.venafi.com/Docs/current/TopNav/Content/SDK/WebSDK/r-SDK-POST-Metadata-DefineItem.php
 
 .LINK
-https://docs.venafi.com/Docs/20.4SDK/TopNav/Content/SDK/WebSDK/r-SDK-POST-Metadata-Item.php
+https://docs.venafi.com/Docs/current/TopNav/Content/SDK/WebSDK/r-SDK-POST-Metadata-Item.php
 
 #>
 
@@ -74,15 +74,15 @@ function New-TppCustomField {
         [Parameter(Mandatory)]
         [ValidateSet('Device', 'X509 Certificate')]
         [ValidateNotNullOrEmpty()]
-        [string]$Class,
+        [string[]]$Class,
 
         [Parameter(Mandatory)]
         [ValidateSet('1', '2', '4', '5')]
         [ValidateNotNullOrEmpty()]
-        [int]$Type,
+        [string]$Type,
 
         [Parameter()]
-        [string]$AllowedValues,
+        [string[]]$AllowedValues,
 
         [Parameter()]
         [string]$Single,
@@ -113,6 +113,9 @@ function New-TppCustomField {
 
         [Parameter()]
         [switch]$TimeOnly,
+
+        [Parameter()]
+        [switch] $PassThru,
 
         [Parameter()]
         [VenafiSession] $VenafiSession = $script:VenafiSession
@@ -161,52 +164,56 @@ function New-TppCustomField {
 
     end {
         if ( $PSCmdlet.ShouldProcess($Label) ) {
-            $response = Invoke-TppRestMethod @params
+            $response = Invoke-VenafiRestMethod @params
 
-            if ( $response.Result -eq [TppMetadataResult]::Success ) {
-                $resultObj = [PSCustomObject] @{
-                "DN"    = $response.Item.DN
-                "Label" = $response.Item.Label
-                "Name"  = $response.Item.Name
-                "Guid"  = $response.Item.Guid
-                "Type"  = $response.Item.Type
-                "Class" = $response.Item.Classes
+            try {
+                if (( $PassThru ) -and ( $response.Result -eq [TppMetadataResult]::Success )) {
+                    $resultObj = [PSCustomObject] @{
+                    "DN"    = $response.Item.DN
+                    "Label" = $response.Item.Label
+                    "Name"  = $response.Item.Name
+                    "Guid"  = [guid]$response.Item.Guid
+                    "Type"  = $response.Item.Type
+                    "Class" = $response.Item.Classes
+                    }
+
+                    if ($response.Item.Mandatory) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "Mandatory" -Value $response.Item.Mandatory
+                    }
+                    if ($response.Item.Policyable) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "Policyable" -Value $response.Item.Policyable
+                    }
+                    if ($response.Item.RenderHidden) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "RenderHidden" -Value $response.Item.RenderHidden
+                    }
+                    if ($response.Item.RenderReadOnly) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "RenderReadOnly" -Value $response.Item.RenderReadOnly
+                    }
+                    if ($response.Item.DateOnly) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "DateOnly" -Value $response.Item.DateOnly
+                    }
+                    if ($response.Item.TimeOnly) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "TimeOnly" -Value $response.Item.TimeOnly
+                    }
+                    if ($response.Item.Help) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "Help" -Value $response.Item.Help
+                    }
+                    if ($response.Item.AllowedValues) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "AllowedValues" -Value $response.Item.AllowedValues
+                    }
+                    if ($response.Item.Single) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "Single" -Value $response.Item.Single
+                    }
+                    if ($response.Item.ErrorMessage) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "ErrorMessage" -Value $response.Item.ErrorMessage
+                    }
+                    if ($response.Item.RegularExpression) {
+                    $resultObj | Add-Member -MemberType NoteProperty -Name "RegularExpression" -Value $response.Item.RegularExpression
+                    }
+
+                    [TppObject] $resultObj
                 }
-                if ($response.Item.Mandatory) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "Mandatory" -Value $response.Item.Mandatory
-                }
-                if ($response.Item.Policyable) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "Policyable" -Value $response.Item.Policyable
-                }
-                if ($response.Item.RenderHidden) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "RenderHidden" -Value $response.Item.RenderHidden
-                }
-                if ($response.Item.RenderReadOnly) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "RenderReadOnly" -Value $response.Item.RenderReadOnly
-                }
-                if ($response.Item.DateOnly) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "DateOnly" -Value $response.Item.DateOnly
-                }
-                if ($response.Item.TimeOnly) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "TimeOnly" -Value $response.Item.TimeOnly
-                }
-                if ($response.Item.Help) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "Help" -Value $response.Item.Help
-                }
-                if ($response.Item.AllowedValues) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "AllowedValues" -Value $response.Item.AllowedValues
-                }
-                if ($response.Item.Single) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "Single" -Value $response.Item.Single
-                }
-                if ($response.Item.ErrorMessage) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "ErrorMessage" -Value $response.Item.ErrorMessage
-                }
-                if ($response.Item.RegularExpression) {
-                $resultObj | Add-Member -MemberType NoteProperty -Name "RegularExpression" -Value $response.Item.RegularExpression
-                }
-                return $resultObj
-            } else {
+            } catch {
                 throw $response.Error
             }
         }
