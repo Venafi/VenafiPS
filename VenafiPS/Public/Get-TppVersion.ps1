969fa1e756df00616e0f4609a376d9546093aa1a
Greg Brownstein <greg@jagtechnical.com>
2022-02-11 13:15:54 -0500

    Update Get-TppVersion.ps1

--- VenafiPS/Public/Get-TppVersion.ps1
+++ VenafiPS/Public/Get-TppVersion.ps1
@@ -47,7 +47,7 @@ function Get-TppVersion {
     }
 
     try {
-        [Version]((Invoke-TppRestMethod @params).Version)
+        [Version]((Invoke-VenafiRestMethod @params).Version)
     }
     catch {
         Throw ("Getting the version failed with the following error: {0}.  This feature was introduced in v18.3." -f $_)
