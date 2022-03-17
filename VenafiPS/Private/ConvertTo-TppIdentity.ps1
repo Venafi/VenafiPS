9307e0d552facbcc039cb2be4370a6656dc0d240
Greg Brownstein <greg@jagtechnical.com>
2022-03-17 11:23:09 -0400

    standard identity format for tpp

---
+++ VenafiPS/Private/ConvertTo-TppIdentity.ps1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+Convert code sign certificate environment to something powershell friendly
+
+.DESCRIPTION
+Convert code sign certificate environment to something powershell friendly
+
+.PARAMETER InputObject
+Code sign certificate environment object
+
+.INPUTS
+InputObject
+
+.OUTPUTS
+PSCustomObject
+
+.EXAMPLE
+$envObj | ConvertTo-TppCodeSignEnvironment
+
+#>
+function ConvertTo-TppIdentity {
+
+    [CmdletBinding()]
+
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [PSCustomObject[]] $InputObject
+    )
+
+    begin {
+    }
+
+    process {
+        $InputObject | Select-Object -Property `
+        @{
+            n = 'Name'
+            e = { $_.Name }
+        },
+        @{
+            n = 'ID'
+            e = { $_.PrefixedUniversal }
+        },
+        @{
+            n = 'Path'
+            e = { $_.FullName }
+        },
+        @{
+            n = 'IsGroup'
+            e = { $_.Type -ne 1 }
+        } -ExcludeProperty PrefixedUniversal, FullName, Prefix, PrefixedName, Type, Universal, IsGroup
+    }
+}
