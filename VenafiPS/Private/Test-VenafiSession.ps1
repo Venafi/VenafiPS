b5dd04e531a238a5f3318627b8bc25e45d1b5169
Greg Brownstein <greg@jagtechnical.com>
2022-04-06 17:25:09 -0400

    help update

--- VenafiPS/Private/Test-VenafiSession.ps1
+++ VenafiPS/Private/Test-VenafiSession.ps1
@@ -1,3 +1,44 @@
+<#
+.SYNOPSIS
+Validate authentication session/key/token
+
+.DESCRIPTION
+Validate authentication session from New-VenafiSession, a VaaS key, or TPP token.
+
+.PARAMETER VenafiSession
+Session object created from New-VenafiSession method.  The value defaults to the script session object $VenafiSession.
+A TPP token or VaaS key can also provided.
+
+.PARAMETER Platform
+Platform, either TPP or Vaas, to validate VenafiSession against.
+
+.PARAMETER AuthType
+Authentication type, either Key or Token, to validate VenafiSession against.
+
+.PARAMETER PassThru
+Provide the determined platform from VenafiSession
+
+.OUTPUTS
+String - if PassThru provided
+
+.EXAMPLE
+Test-VenafiSession -VenafiSession $VenafiSession
+Test a session
+
+.EXAMPLE
+Test-VenafiSession -VenafiSession $key
+Test a VaaS key
+
+.EXAMPLE
+Test-VenafiSession -VenafiSession $VenafiSession -Platform TPP
+Test session ensuring the platform is TPP
+
+.EXAMPLE
+Test-VenafiSession -VenafiSession $VenafiSession -Platform TPP -AuthType Token
+Test session ensuring the platform is TPP and authentication type is token
+
+#>
+
 function Test-VenafiSession {
 
     [CmdletBinding(DefaultParameterSetName = 'All')]
@@ -9,14 +50,15 @@ function Test-VenafiSession {
 
         [Parameter(Mandatory, ParameterSetName = 'Platform')]
         [Parameter(Mandatory, ParameterSetName = 'AuthType')]
+        [Alias('VaaS', 'TPP')]
         [string] $Platform,
 
         [Parameter(Mandatory, ParameterSetName = 'AuthType')]
+        [Alias('Key', 'Token')]
         [string] $AuthType,
 
         [Parameter()]
         [switch] $PassThru
-
     )
 
     process {
