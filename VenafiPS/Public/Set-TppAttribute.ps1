fd3fd0e76cdfadd81bcf04d91b9bdd62750cfd44
Greg Brownstein <greg@jagtechnical.com>
2022-07-15 06:45:42 -0400

    policy custom field fix (#132)

--- VenafiPS/Public/Set-TppAttribute.ps1
+++ VenafiPS/Public/Set-TppAttribute.ps1
@@ -16,7 +16,7 @@ function Set-TppAttribute {
     To clear a value overwriting policy, set the value to $null.
 
     .PARAMETER BypassValidation
-    Bypass data validation.  Only appicable to custom fields.
+    Bypass data validation.  Only applicable to custom fields.
 
     .PARAMETER Policy
     Set policies (aka policy attributes) instead of object attributes
@@ -43,7 +43,7 @@ function Set-TppAttribute {
     .EXAMPLE
     Set-TppAttribute -Path '\VED\Policy\My Folder\app.company.com' -Attribute @{'Consumers'='\VED\Policy\myappobject.company.com'}
 
-    Set a value on an object
+    Set the value on an object
 
     .EXAMPLE
     Set-TppAttribute -Path '\VED\Policy\My Folder\app.company.com' -Attribute @{'Management Type'=$null}
@@ -53,20 +53,20 @@ function Set-TppAttribute {
     .EXAMPLE
     Set-TppAttribute -Path '\VED\Policy\My Folder\app.company.com' -Attribute @{'My custom field Label'='new custom value'}
 
-    Set value on custom field
+    Set the value on a custom field
 
     .EXAMPLE
     Set-TppAttribute -Path '\VED\Policy\My Folder\app.company.com' -Attribute @{'My custom field Label'='new custom value'} -BypassValidation
 
-    Set value on custom field bypassing field validation
+    Set the value on a custom field bypassing field validation
 
     .EXAMPLE
-    Set-TppAttribute -Path '\VED\Policy\My Folder' -Policy -ClassName 'X509 Certificate' -Attribute @{'Notification Disabled'='0'}
+    Set-TppAttribute -Path '\VED\Policy\My Folder' -PolicyClass 'X509 Certificate' -Attribute @{'Notification Disabled'='0'}
 
     Set a policy attribute
 
     .EXAMPLE
-    Set-TppAttribute -Path '\VED\Policy\My Folder' -Policy -ClassName 'X509 Certificate' -Attribute @{'Notification Disabled'='0'} -Lock
+    Set-TppAttribute -Path '\VED\Policy\My Folder' -PolicyClass 'X509 Certificate' -Attribute @{'Notification Disabled'='0'} -Lock
 
     Set a policy attribute and lock the value
 
@@ -80,13 +80,16 @@ function Set-TppAttribute {
     https://docs.venafi.com/Docs/current/TopNav/Content/SDK/WebSDK/r-SDK-POST-Metadata-Set.php
 
     .LINK
+    https://docs.venafi.com/Docs/currentSDK/TopNav/Content/SDK/WebSDK/r-SDK-POST-Metadata-SetPolicy.php
+
+    .LINK
     https://docs.venafi.com/Docs/current/TopNav/Content/SDK/WebSDK/r-SDK-POST-Config-write.php
 
     .LINK
     https://docs.venafi.com/Docs/current/TopNav/Content/SDK/WebSDK/r-SDK-POST-Config-writepolicy.php
     #>
 
-    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'Object')]
+    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'NotPolicy')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '', Justification = 'Being flagged incorrectly')]
 
     param (
@@ -105,14 +108,15 @@ function Set-TppAttribute {
         [Parameter(Mandatory)]
         [hashtable] $Attribute,
 
-        [Parameter(ParameterSetName = 'Object')]
+        [Parameter()]
         [switch] $BypassValidation,
 
-        [Parameter(Mandatory, ParameterSetName = 'Policy')]
+        [Parameter(ParameterSetName = 'Policy')]
         [switch] $Policy,
 
         [Parameter(Mandatory, ParameterSetName = 'Policy')]
-        [string] $ClassName,
+        [Alias('ClassName')]
+        [string] $PolicyClass,
 
         [Parameter(ParameterSetName = 'Policy')]
         [switch] $Lock,
@@ -124,73 +128,72 @@ function Set-TppAttribute {
     begin {
         Test-VenafiSession -VenafiSession $VenafiSession -Platform 'TPP'
 
+        if ( $Policy ) { Write-Warning '-Policy is no longer required; just provide -PolicyClass to set policy attributes.' }
+
         $params = @{
             VenafiSession = $VenafiSession
             Method        = 'Post'
         }
 
-        if ( -not $Policy ) {
+        $baseFields = @()
+        $customFields = @()
 
-            $baseFields = @()
-            $customFields = @()
+        $Attribute.GetEnumerator() | ForEach-Object {
 
-            $Attribute.GetEnumerator() | ForEach-Object {
+            $thisKey = $_.Key
+            $thisValue = $_.Value
+            $customFieldError = $null
 
-                $thisKey = $_.Key
-                $thisValue = $_.Value
-                $customFieldError = $null
-
-                $customField = $VenafiSession.CustomField | Where-Object { $_.Label -eq $thisKey -or $_.Guid -eq $thisKey }
-                if ( $customField ) {
-                    switch ( $customField.Type.ToString() ) {
-                        '1' {
-                            # string
-                            if ( $customField.RegularExpression -and $thisValue -notmatch $customField.RegularExpression ) {
-                                $customFieldError = 'regular expression ''{0}'' validation failed' -f $customField.RegularExpression
-                            }
-                        }
-
-                        '2' {
-                            # list
-                            if ( $thisValue -notin $customField.AllowedValues ) {
-                                $customFieldError = 'value is not in the list of allowed values ''{0}''' -f $customField.AllowedValues
-                            }
-                        }
-
-                        '5' {
-                            # identity
-                            if ( -not ($thisValue | Test-TppIdentity -ExistOnly -VenafiSession $VenafiSession) ) {
-                                $customFieldError = 'value is not a valid identity'
-                            }
-                        }
-
-                        '4' {
-                            # date/time
-                            try {
-                                [datetime] $thisValue
-                            } catch {
-                                $customFieldError = 'value is not a valid date'
-                            }
-                        }
-
-                        Default {
-                            $customFieldError = 'unknown custom field type'
+            $customField = $VenafiSession.CustomField | Where-Object { $_.Label -eq $thisKey -or $_.Guid -eq $thisKey }
+            if ( $customField ) {
+                switch ( $customField.Type.ToString() ) {
+                    '1' {
+                        # string
+                        if ( $customField.RegularExpression -and $thisValue -notmatch $customField.RegularExpression ) {
+                            $customFieldError = 'regular expression ''{0}'' validation failed' -f $customField.RegularExpression
                         }
                     }
 
-                    if ( $customFieldError -and -not $BypassValidation.IsPresent ) {
-                        Write-Error ('The value ''{0}'' for field ''{1}'' encountered an error, {2}' -f $thisValue, $thisKey, $customFieldError)
-                    } else {
-                        $customFields += @{
-                            ItemGuid = $customField.Guid
-                            List     = if ($null -eq $thisValue) { , @() } else { , @($thisValue) }
+                    '2' {
+                        # list
+                        if ( $thisValue -notin $customField.AllowedValues ) {
+                            $customFieldError = 'value is not in the list of allowed values ''{0}''' -f $customField.AllowedValues
                         }
                     }
+
+                    '5' {
+                        # identity
+                        if ( -not ($thisValue | Test-TppIdentity -ExistOnly -VenafiSession $VenafiSession) ) {
+                            $customFieldError = 'value is not a valid identity'
+                        }
+                    }
+
+                    '4' {
+                        # date/time
+                        try {
+                            [datetime] $thisValue
+                        } catch {
+                            $customFieldError = 'value is not a valid date'
+                        }
+                    }
+
+                    Default {
+                        $customFieldError = 'unknown custom field type'
+                    }
+                }
+
+                if ( $customFieldError -and -not $BypassValidation ) {
+                    Write-Error ('The value ''{0}'' for field ''{1}'' encountered an error, {2}' -f $thisValue, $thisKey, $customFieldError)
                 } else {
-                    $baseFields += @{
-                        Name  = $thisKey
-                        Value = if ($null -eq $thisValue) { , @() } else { , @($thisValue) }
+                    $customFields += @{
+                        ItemGuid = $customField.Guid
+                        List     = if ($null -eq $thisValue) { , @() } else { , @($thisValue) }
                     }
+                }
+            } else {
+                $baseFields += @{
+                    Name  = $thisKey
+                    Value = if ($null -eq $thisValue) { , @() } else { , @($thisValue) }
                 }
             }
         }
@@ -202,16 +205,36 @@ function Set-TppAttribute {
             continue
         }
 
-        if ( $Policy ) {
-            $Attribute.GetEnumerator() | ForEach-Object {
+        # built-in fields and custom fields have different APIs and payloads
+        # as do attributes and policy attributes...
 
-                $params.UriLeaf = 'config/WritePolicy'
+        if ( $baseFields.Count -gt 0 ) {
+
+            if ( $PSBoundParameters.ContainsKey('PolicyClass') ) {
+                # config/WritePolicy only allows 1 key/value per call
+                foreach ($field in $baseFields) {
+
+                    $params.UriLeaf = 'config/WritePolicy'
+                    $params.Body = @{
+                        ObjectDN      = $Path
+                        Class         = $PolicyClass
+                        AttributeName = $field.Name
+                        Values        = @($field.Value)
+                        Locked        = [int]$Lock.ToBool()
+                    }
+
+                    $response = Invoke-VenafiRestMethod @params
+
+                    if ( $response.Result -ne [TppConfigResult]::Success ) {
+                        Write-Error $response.Error
+                    }
+                }
+            } else {
+
+                $params.UriLeaf = 'config/Write'
                 $params.Body = @{
                     ObjectDN      = $Path
-                    Class         = $ClassName
-                    AttributeName = $_.Key
-                    Values        = @($_.Value)
-                    Locked        = [int]$Lock.ToBool()
+                    AttributeData = $baseFields
                 }
 
                 $response = Invoke-VenafiRestMethod @params
@@ -222,35 +245,29 @@ function Set-TppAttribute {
             }
         }
 
-        # built-in fields and custom fields have different APIs and payloads
-
-        if ( $baseFields.Count -gt 0 ) {
-
-            $params.UriLeaf = 'config/Write'
-            $params.Body = @{
-                ObjectDN      = $Path
-                AttributeData = $baseFields
-            }
-
-            $response = Invoke-VenafiRestMethod @params
-
-            if ( $response.Result -ne [TppConfigResult]::Success ) {
-                Write-Error $response.Error
-            }
-        }
-
         if ( $customFields.Count -gt 0 ) {
 
-            $params.UriLeaf = 'Metadata/Set'
-            $params.Body = @{
-                DN           = $Path
-                GuidData     = $customFields
-                KeepExisting = $true
+            if ( $PSBoundParameters.ContainsKey('PolicyClass') ) {
+                $params.UriLeaf = 'Metadata/SetPolicy'
+                $params.Body = @{
+                    DN          = $Path
+                    ConfigClass = $PolicyClass
+                    GuidData    = $customFields
+                    Locked      = [int]$Lock.ToBool()
+                }
+            } else {
+
+                $params.UriLeaf = 'Metadata/Set'
+                $params.Body = @{
+                    DN           = $Path
+                    GuidData     = $customFields
+                    KeepExisting = $true
+                }
             }
 
             $response = Invoke-VenafiRestMethod @params
 
-            if ( $response.Result -ne 0 ) {
+            if ( $response.Result -ne [TppMetadataResult]::Success ) {
                 Write-Error $response.Error
             }
         }
