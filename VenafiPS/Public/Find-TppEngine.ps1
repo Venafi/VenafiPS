2b284023a19bef053d67be9a6673e5f7fc4bc348
Christopher <81055999+ccamacho1966@users.noreply.github.com>
2022-09-14 22:21:20 -0400

    replace with gdbarron suggested version
    Co-authored-by: Greg Brownstein <greg@jagtechnical.com>

--- VenafiPS/Public/Find-TppEngine.ps1
+++ VenafiPS/Public/Find-TppEngine.ps1
@@ -1,38 +1,42 @@
-<#
-.SYNOPSIS
-Find TPP engines using an optional pattern
-.DESCRIPTION
-Find TPP engines using an optional pattern.
-This function is an engine wrapper for Find-TppObject.
-.PARAMETER Pattern
-Filter against engine names using asterisk (*) and/or question mark (?) wildcard characters.
-.PARAMETER VenafiSession
-Authentication for the function.
-The value defaults to the script session object $VenafiSession created by New-VenafiSession.
-A TPP token can also provided, but this requires an environment variable TPP_SERVER to be set.
-.INPUTS
-Pattern
-.OUTPUTS
-TppObject
-.EXAMPLE
-Find-TppEngine
-Get an array of all TPP engines
-.EXAMPLE
-Find-TppEngine -Pattern '*partialname*'
-Get engines whose name matches the supplied pattern
-.LINK
-http://VenafiPS.readthedocs.io/en/latest/functions/Find-TppEngine/
-.LINK
-https://github.com/Venafi/VenafiPS/blob/main/VenafiPS/Public/Find-TppEngine.ps1
-#>
 function Find-TppEngine {
+    <#
+    .SYNOPSIS
+    Find TPP engines using an optional pattern
+
+    .DESCRIPTION
+    Find TPP engines using an optional pattern.
+    This function is an engine wrapper for Find-TppObject.
+
+    .PARAMETER Pattern
+    Filter against engine names using asterisk (*) and/or question mark (?) wildcard characters.
+
+    .PARAMETER VenafiSession
+    Authentication for the function.
+    The value defaults to the script session object $VenafiSession created by New-VenafiSession.
+    A TPP token can also provided, but this requires an environment variable TPP_SERVER to be set.
+
+    .INPUTS
+    Pattern
+
+    .OUTPUTS
+    TppObject
+
+    .EXAMPLE
+    Find-TppEngine -Pattern '*partialname*'
+
+    Get engines whose name matches the supplied pattern
+
+    .LINK
+    http://VenafiPS.readthedocs.io/en/latest/functions/Find-TppEngine/
+
+    .LINK
+    https://github.com/Venafi/VenafiPS/blob/main/VenafiPS/Public/Find-TppEngine.ps1
+    #>
 
     [CmdletBinding()]
 
     param (
-        [Parameter(ValueFromPipeline)]
-        [ValidateNotNull()]
-        [AllowEmptyString()]
+        [Parameter(Mandatory, ValueFromPipeline)]
         [String] $Pattern,
 
         [Parameter()]
@@ -48,13 +52,9 @@ function Find-TppEngine {
             VenafiSession = $VenafiSession
             Class         = 'Venafi Platform'
             Path          = '\VED\Engines'
+            Pattern       = $Pattern
         }
 
-        # add filters
-        if ( $PSBoundParameters.ContainsKey('Pattern') ) {
-            $params.Pattern = $Pattern
-        }
-        
         Find-TppObject @params
     }
 }
