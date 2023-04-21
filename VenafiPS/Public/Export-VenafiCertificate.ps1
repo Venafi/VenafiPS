ac2577e10c4e9392b90465050d246e5996b1bff3
Greg Brownstein <greg@jagtechnical.com>
2023-04-21 16:30:46 -0400

    add vaas chain export and to file (#194)

--- VenafiPS/Public/Export-VenafiCertificate.ps1
+++ VenafiPS/Public/Export-VenafiCertificate.ps1
@@ -9,19 +9,23 @@ function Export-VenafiCertificate {
     .PARAMETER CertificateId
     Certificate identifier.  For Venafi as a Service, this is the unique guid.  For TPP, use the full path.
 
-    .PARAMETER Format
-    Certificate format.
-    For Venafi as a Service, you can provide either PEM, DER, or JKS.
-    For TPP, you can provide Base64, Base64 (PKCS#8), DER, JKS, PKCS #7, or PKCS #12.
+    .PARAMETER TppFormat
+    Certificate format, either Base64, Base64 (PKCS#8), DER, PKCS #7, or PKCS #12.
+
+    .PARAMETER VaasFormat
+    Certificate format, either DER or PEM
 
     .PARAMETER OutPath
-    Folder path to save the certificate to.  The name of the file will be determined automatically.  TPP Only...for now.
+    Folder path to save the certificate to.  The name of the file will be determined automatically.
 
     .PARAMETER IncludeChain
-    Include the certificate chain with the exported certificate.  Not supported with DER format.  TPP Only.
+    Include the certificate chain with the exported certificate.  Not supported with DER format.
+
+    .PARAMETER RootFirst
+    Use with -IncludeChain for VaaS to return the root first instead of the end entity first
 
     .PARAMETER FriendlyName
-    Label or alias to use.  Permitted with Base64 and PKCS #12 formats.  Required when Format is JKS.  TPP Only.
+    Label or alias to use.  Permitted with Base64 and PKCS #12 formats.  Required when exporting JKS.  TPP Only.
 
     .PARAMETER IncludePrivateKey
     DEPRECATED. Provide a value for -PrivateKeyPassword.  TPP only.
@@ -59,47 +63,61 @@ function Export-VenafiCertificate {
     Vaas, System.String.  TPP, PSCustomObject.
 
     .EXAMPLE
-    $certId | Export-VenafiCertificate -Format PEM
+    $certId | Export-VenafiCertificate -VaasFormat PEM
     Get certificate data from Venafi as a Service
 
     .EXAMPLE
-    $cert | Export-VenafiCertificate -Format 'PKCS #7' -OutPath 'c:\temp'
-    Get certificate data and save to a file, TPP
+    $cert | Export-VenafiCertificate -TppFormat 'PKCS #7' -OutPath 'c:\temp'
+    Get certificate data and save to a file
 
     .EXAMPLE
-    $cert | Export-VenafiCertificate -Format 'PKCS #7' -IncludeChain
+    $cert | Export-VenafiCertificate -TppFormat 'PKCS #7' -IncludeChain
     Get one or more certificates with the certificate chain included, TPP
 
     .EXAMPLE
-    $cert | Export-VenafiCertificate -Format 'PKCS #12' -PrivateKeyPassword $cred.password
+    $cert | Export-VenafiCertificate -VaasFormat PEM -IncludeChain -RootFirst
+    Get one or more certificates with the certificate chain included and the root first in the chain, VaaS
+
+    .EXAMPLE
+    $cert | Export-VenafiCertificate -TppFormat 'PKCS #12' -PrivateKeyPassword $cred.password
     Get one or more certificates with private key included, TPP
 
     .EXAMPLE
     $cert | Export-VenafiCertificate -FriendlyName 'MyFriendlyName' -KeystorePassword $cred.password
-    Get certificates in JKS format, TPP
+    Get certificates in JKS format, TPP.  -TppFormat not needed since we know its JKS via -KeystorePassword.
 
     #>
 
-    [CmdletBinding(DefaultParameterSetName = 'All')]
-    [Alias('Get-TppCertificate', 'Export-TppCertificate', 'Export-VaasCertificate')]
+    [CmdletBinding()]
 
     param (
 
-        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [Parameter(ParameterSetName = 'Tpp', Mandatory, ValueFromPipelineByPropertyName)]
+        [Parameter(ParameterSetName = 'TppJks', Mandatory, ValueFromPipelineByPropertyName)]
+        [Parameter(ParameterSetName = 'Vaas', Mandatory, ValueFromPipelineByPropertyName)]
+        [Parameter(ParameterSetName = 'VaasChain', Mandatory, ValueFromPipelineByPropertyName)]
         [Alias('Path', 'id')]
         [string] $CertificateId,
 
-        [Parameter(Mandatory, ParameterSetName = 'All')]
-        [Parameter(Mandatory, ParameterSetName = 'Tpp')]
-        [ValidateSet("Base64", "Base64 (PKCS #8)", "DER", "JKS", "PKCS #7", "PKCS #12", "PEM")]
-        [string] $Format,
+        [Parameter(ParameterSetName = 'Tpp', Mandatory)]
+        [ValidateSet("Base64", "Base64 (PKCS #8)", "DER", "PKCS #7", "PKCS #12")]
+        [Alias('Format')]
+        [string] $TppFormat,
+
+        [Parameter(ParameterSetName = 'Vaas', Mandatory)]
+        [Parameter(ParameterSetName = 'VaasChain', Mandatory)]
+        [ValidateSet("DER", "PEM")]
+        [string] $VaasFormat,
 
         [Parameter(ParameterSetName = 'Tpp')]
+        [Parameter(ParameterSetName = 'Vaas')]
+        [Parameter(ParameterSetName = 'VaasChain')]
         [ValidateNotNullOrEmpty()]
         [ValidateScript( {
                 if (Test-Path $_ -PathType Container) {
                     $true
-                } else {
+                }
+                else {
                     Throw "Output path '$_' does not exist"
                 }
             })]
@@ -107,14 +125,15 @@ function Export-VenafiCertificate {
 
         [Parameter(ParameterSetName = 'Tpp')]
         [Parameter(ParameterSetName = 'TppJks')]
+        [Parameter(ParameterSetName = 'VaasChain', Mandatory)]
         [switch] $IncludeChain,
+
+        [Parameter(ParameterSetName = 'VaasChain')]
+        [switch] $RootFirst,
 
         [Parameter(ParameterSetName = 'Tpp')]
         [Parameter(Mandatory, ParameterSetName = 'TppJks')]
         [string] $FriendlyName,
-
-        [Parameter(ParameterSetName = 'Tpp')]
-        [switch] $IncludePrivateKey,
 
         [Parameter(ParameterSetName = 'Tpp')]
         [Parameter(ParameterSetName = 'TppJks')]
@@ -129,32 +148,37 @@ function Export-VenafiCertificate {
     )
 
     begin {
-        $platform = Test-VenafiSession -VenafiSession $VenafiSession -PassThru
-
-        if ( $PsCmdlet.ParameterSetName -ne 'All' -and $PsCmdlet.ParameterSetName -notmatch "^$platform" ) {
-            throw "The parameters selected are not applicable to $platform"
-        }
+        $platform = Test-VenafiSession -VenafiSession $VenafiSession -Platform $PsCmdlet.ParameterSetName -PassThru
 
         $params = @{
             VenafiSession = $VenafiSession
-            Body          = @{
-                Format = $Format
-            }
+            Body          = @{}
         }
 
         if ( $platform -eq 'VaaS' ) {
 
-            if ( $Format -notin 'PEM', 'DER', 'JKS') {
-                throw 'VaaS supports format PEM, DER, and JKS'
+            $params.Body.Format = $VaasFormat
+
+            if ( $IncludeChain ) {
+                if ( $Format -in @('DER') ) {
+                    throw "-IncludeChain is not supported with the DER Format"
+                }
+
+                $params.Body.chainOrder = 'EE_FIRST'
+                if ( $RootFirst ) {
+                    $params.Body.chainOrder = 'ROOT_FIRST'
+                }
+            }
+            else {
+                $params.Body.chainOrder = 'EE_ONLY'
             }
 
-        } else {
+        }
+        else {
 
-            if ( $Format -eq 'PEM') {
-                throw 'TPP supports format Base64, Base64 (PKCS #8), DER, JKS, PKCS #7, and PKCS #12'
-            }
+            $params.Body.Format = $TppFormat
 
-            if ($PrivateKeyPassword) {
+            if ( $PSBoundParameters.ContainsKey('PrivateKeyPassword') ) {
 
                 # validate format to be able to export the private key
                 if ( $Format -in @("DER", "PKCS #7") ) {
@@ -166,32 +190,26 @@ function Export-VenafiCertificate {
                 $params.Body.Password = $plainTextPassword
             }
 
-            if ($Format -in @("Base64 (PKCS #8)", "DER", "PKCS #7")) {
-                if (-not ([string]::IsNullOrEmpty($FriendlyName))) {
-                    throw "Only Base64, JKS, PKCS #12 formats support FriendlyName parameter"
-                }
-            }
-
-            if ( $KeystorePassword ) {
+            if ( $PSBoundParameters.ContainsKey('KeystorePassword') ) {
                 $params.Body.Format = 'JKS'
                 $plainTextPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($KeystorePassword))
                 $params.Body.KeystorePassword = $plainTextPassword
             }
 
-            if (-not [string]::IsNullOrEmpty($FriendlyName)) {
+            if ( $PSBoundParameters.ContainsKey('FriendlyName') ) {
+                if ($Format -in @("Base64 (PKCS #8)", "DER", "PKCS #7")) {
+                    throw "Only Base64, JKS, PKCS #12 formats support -FriendlyName parameter"
+                }
+
                 $params.Body.FriendlyName = $FriendlyName
             }
 
             if ($IncludeChain) {
                 if ( $Format -in @('DER') ) {
-                    throw "IncludeChain is not supported with the DER Format"
+                    throw "-IncludeChain is not supported with the DER Format"
                 }
 
                 $params.Body.IncludeChain = $true
-            }
-
-            if ($IncludePrivateKey) {
-                Write-Warning "IncludePrivateKey is DEPRECATED. Provide a value for just -PrivateKeyPassword instead."
             }
         }
     }
@@ -204,11 +222,13 @@ function Export-VenafiCertificate {
             $params.Method = 'Get'
             $params.FullResponse = $true
             $response = Invoke-VenafiRestMethod @params
-            [pscustomobject] @{
-                'CertificateData' = $response.Content -replace "`r|`n|-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----"
+            $response = [pscustomobject] @{
+                'CertificateData' = $response.Content -replace "`r|`n|-----(BEGIN|END)[\w\s]+-----|\\r|\\n"
+                FileName          = "$CertificateId.$VaasFormat"
             }
 
-        } else {
+        }
+        else {
             $params.Method = 'Post'
             $params.UriLeaf = 'certificates/retrieve'
 
@@ -217,17 +237,18 @@ function Export-VenafiCertificate {
             $response = Invoke-VenafiRestMethod @params
 
             Write-Verbose ($response | Format-List | Out-String)
+        }
 
-            if ( $PSBoundParameters.ContainsKey('OutPath') ) {
-                if ( $response.PSobject.Properties.name -contains "CertificateData" ) {
-                    $outFile = Join-Path -Path $OutPath -ChildPath ($response.FileName.Trim('"'))
-                    $bytes = [Convert]::FromBase64String($response.CertificateData)
-                    [IO.File]::WriteAllBytes($outFile, $bytes)
-                    Write-Verbose ('Saved {0} with format {1}' -f $outFile, $response.Format)
-                }
-            } else {
-                $response
+        if ( $PSBoundParameters.ContainsKey('OutPath') ) {
+            if ( $response.PSobject.Properties.name -contains "CertificateData" ) {
+                $outFile = Join-Path -Path $OutPath -ChildPath ($response.FileName.Trim('"'))
+                $bytes = [Convert]::FromBase64String($response.CertificateData)
+                [IO.File]::WriteAllBytes($outFile, $bytes)
+                Write-Verbose "Saved $outFile"
             }
+        }
+        else {
+            $response
         }
     }
 }
