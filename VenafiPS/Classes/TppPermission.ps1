6b9e9837f3e6bef8b29ba5a24bd9f9b690f92d05
Greg Brownstein <greg.brownstein@venafi.com>
2023-05-17 13:44:44 -0400

    fix identity deleted error

--- VenafiPS/Classes/TppPermission.ps1
+++ VenafiPS/Classes/TppPermission.ps1
@@ -13,10 +13,25 @@ class TppPermission {
     [bool] $IsViewAllowed
     [bool] $IsWriteAllowed
 
+    TppPermission ([pscustomobject] $InputObject) {
+        $this.IsAssociateAllowed = $InputObject.IsAssociateAllowed
+        $this.IsCreateAllowed = $InputObject.IsCreateAllowed
+        $this.IsDeleteAllowed = $InputObject.IsDeleteAllowed
+        $this.IsManagePermissionsAllowed = $InputObject.IsManagePermissionsAllowed
+        $this.IsPolicyWriteAllowed = $InputObject.IsPolicyWriteAllowed
+        $this.IsPrivateKeyReadAllowed = $InputObject.IsPrivateKeyReadAllowed
+        $this.IsPrivateKeyWriteAllowed = $InputObject.IsPrivateKeyWriteAllowed
+        $this.IsReadAllowed = $InputObject.IsReadAllowed
+        $this.IsRenameAllowed = $InputObject.IsRenameAllowed
+        $this.IsRevokeAllowed = $InputObject.IsRevokeAllowed
+        $this.IsViewAllowed = $InputObject.IsViewAllowed
+        $this.IsWriteAllowed = $InputObject.IsWriteAllowed
+    }
+
     [HashTable] ToHashtable() {
 
         $hash = @{}
-        $propNames = $this | Get-Member | Where-Object {$_.MemberType -eq 'Property'} | Select-Object -ExpandProperty Name
+        $propNames = $this | Get-Member | Where-Object { $_.MemberType -eq 'Property' } | Select-Object -ExpandProperty Name
 
         foreach ($prop in $propNames) {
             if ($this.GetType().GetProperty($prop)) {
