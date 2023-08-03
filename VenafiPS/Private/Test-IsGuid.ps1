e906573506e66aaa842e793ead3b3b33808de2ec
Greg Brownstein <greg.brownstein@venafi.com>
2023-08-03 10:39:35 -0400

    test guid new function

---
+++ VenafiPS/Private/Test-IsGuid.ps1
@@ -0,0 +1,23 @@
+function Test-IsGuid {
+    <#
+    .SYNOPSIS
+    Validates a given input string and checks string is a valid GUID
+    .DESCRIPTION
+    Validates a given input string and checks string is a valid GUID by using the .NET method Guid.TryParse
+    .EXAMPLE
+    Test-Guid -InputObject "3363e9e1-00d8-45a1-9c0c-b93ee03f8c13"
+    .NOTES
+    Uses .NET method [guid]::TryParse()
+    #>
+    [Cmdletbinding()]
+    [OutputType([bool])]
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [AllowEmptyString()]
+        [string] $InputObject
+    )
+    process {
+        return [guid]::TryParse($InputObject, $([ref][guid]::Empty))
+    }
+}
