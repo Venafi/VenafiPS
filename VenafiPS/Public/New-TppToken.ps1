2e146d9717ac88e2742915b33dd539e5e69a9084
Greg Brownstein <greg.brownstein@venafi.com>
2023-08-30 11:30:15 -0400

    more codesign cleanup

--- VenafiPS/Public/New-TppToken.ps1
+++ VenafiPS/Public/New-TppToken.ps1
@@ -21,10 +21,8 @@ function New-TppToken {
     A privilege restriction of none or read, use a value of $null.
     Scopes include Agent, Certificate, Code Signing, Configuration, Restricted, Security, SSH, and statistics.
     See https://docs.venafi.com/Docs/current/TopNav/Content/SDK/AuthSDK/r-SDKa-OAuthScopePrivilegeMapping.php
-    Using a scope of {'all'='core'} will set all scopes except for codesignclient and admin.
-    Using a scope of {'all'='core-cs'} will set all scopes inclduing codesignclient except for admin.
+    Using a scope of {'all'='core'} will set all scopes except for admin.
     Using a scope of {'all'='admin'} will set all scopes including admin.
-    Using a scope of {'all'='admin-cs'} will set all scopes including admin.
     Usage of the 'all' scope is not suggested for production.
 
     .PARAMETER Credential
@@ -194,19 +192,11 @@ function New-TppToken {
             # obtain new token
             $scopeString = switch ($Scope.all) {
                 'core' {
-                    'agent:delete;certificate:approve,delete,discover,manage,revoke;configuration:delete,manage;restricted:delete,manage;security:delete,manage;ssh:approve,delete,discover,manage;statistics;codesign:delete,manage'
-                }
-
-                'core-cs' {
-                    'agent:delete;certificate:approve,delete,discover,manage,revoke;configuration:delete,manage;restricted:delete,manage;security:delete,manage;ssh:approve,delete,discover,manage;statistics;codesign:delete,manage;codesignclient'
+                    'agent:delete;certificate:approve,delete,discover,manage,revoke;configuration:delete,manage;restricted:delete,manage;security:delete,manage;ssh:approve,delete,discover,manage;statistics'
                 }
 
                 'admin' {
-                    'admin:delete,viewlogs,recyclebin;agent:delete;certificate:delete,discover,manage,revoke;configuration:delete,manage;restricted:delete,manage;security:delete,manage;ssh:approve,delete,discover,manage;statistics;codesign:approve,admin,delete,manage'
-                }
-
-                'admin-cs' {
-                    'admin:delete,viewlogs,recyclebin;agent:delete;certificate:delete,discover,manage,revoke;configuration:delete,manage;restricted:delete,manage;security:delete,manage;ssh:approve,delete,discover,manage;statistics;codesign:approve,admin,delete,manage;codesignclient'
+                    'admin:delete,viewlogs,recyclebin;agent:delete;certificate:delete,discover,manage,revoke;configuration:delete,manage;restricted:delete,manage;security:delete,manage;ssh:approve,delete,discover,manage;statistics'
                 }
 
                 Default {
