2583f0d9728cb35f296a2d2ea6b88e737c8436fa
Greg Brownstein <greg@jagtechnical.com>
2024-02-13 17:06:33 -0500

    remove references to deprecated parameter -VaultMetadata (#257)

--- VenafiPS/Public/Test-VdcToken.ps1
+++ VenafiPS/Public/Test-VdcToken.ps1
@@ -18,8 +18,6 @@ function Test-VdcToken {
 
     .PARAMETER VaultAccessTokenName
     Name of the SecretManagement vault entry for the access token; the name of the vault must be VenafiPS.
-    Note: '-Server' parameter is required if the vault does not contain saved metadata.
-    See New-VenafiSession -VaultMetaData
 
     .PARAMETER VenafiPsToken
     Token object obtained from New-VdcToken
@@ -188,7 +186,7 @@ function Test-VdcToken {
                 }
                 else {
                     if ( -not $AuthServer ) {
-                        throw '-AuthServer is a required parameter as it wasn''t stored with New-VenafiSession -VaultMetadata'
+                        throw '-AuthServer is a required parameter'
                     }
 
                     $params.Server = $serverUrl
