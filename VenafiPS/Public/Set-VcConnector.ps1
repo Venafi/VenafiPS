a2a85f78e74ef3965ee945790fc3e5c9b3e64674
Greg Brownstein <greg.brownstein@venafi.com>
2024-03-06 16:52:49 -0500

    typo

--- VenafiPS/Public/Set-VcConnector.ps1
+++ VenafiPS/Public/Set-VcConnector.ps1
@@ -34,12 +34,12 @@
     Update an existing connector with the same name as in the manifest
 
     .EXAMPLE
-    Set-VcConnector ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2' -ManifestPath '/tmp/manifest_v2.json'
+    Set-VcConnector -ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2' -ManifestPath '/tmp/manifest_v2.json'
 
     Update an existing connector utilizing a specific connector ID
 
     .EXAMPLE
-    Set-VcConnector ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2' -Disable
+    Set-VcConnector -ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2' -Disable
 
     Disable a connector
 
@@ -49,7 +49,7 @@
     Disable a connector by name
 
     .EXAMPLE
-    Set-VcConnector ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2' -Disable:$false
+    Set-VcConnector -ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2' -Disable:$false
 
     Reenable a disabled connector
 
