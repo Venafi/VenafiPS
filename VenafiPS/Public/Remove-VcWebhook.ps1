225b2ee080eded171e3806e1d1476245fced249d
Greg Brownstein <greg@jagtechnical.com>
2024-03-06 16:57:52 -0500

    Support tenant level connectors on TLSPC (#263)

---
+++ VenafiPS/Public/Remove-VcWebhook.ps1
@@ -0,0 +1,60 @@
+﻿function Remove-VcWebhook {
+    <#
+    .SYNOPSIS
+    Remove a webhook
+
+    .DESCRIPTION
+    Remove a webhook from TLSPC
+
+    .PARAMETER ID
+    Webhook ID, this is the guid/uuid
+
+    .PARAMETER VenafiSession
+    Authentication for the function.
+    The value defaults to the script session object $VenafiSession created by New-VenafiSession.
+    A TLSPC key can also provided.
+
+    .INPUTS
+    ID
+
+    .EXAMPLE
+    Remove-VcWebhook -ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2'
+    Remove a webhook
+
+    .EXAMPLE
+    Remove-VcWebhook -ID 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2' -Confirm:$false
+    Remove a webhook bypassing the confirmation prompt
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+
+    param (
+
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias('webhookId')]
+        [string] $ID,
+
+        [Parameter()]
+        [int32] $ThrottleLimit = 100,
+
+        [Parameter()]
+        [psobject] $VenafiSession
+    )
+
+    begin {
+        Test-VenafiSession -VenafiSession $VenafiSession -Platform 'VC'
+        $allObjects = [System.Collections.Generic.List[object]]::new()
+    }
+
+    process {
+        if ( $PSCmdlet.ShouldProcess($ID, "Delete Webhook") ) {
+            $allObjects.Add($ID)
+        }
+    }
+
+    end {
+        Invoke-VenafiParallel -InputObject $allObjects -ScriptBlock {
+            $null = Invoke-VenafiRestMethod -Method 'Delete' -UriLeaf "connectors/$PSItem"
+        } -ThrottleLimit $ThrottleLimit -VenafiSession $VenafiSession
+    }
+}
