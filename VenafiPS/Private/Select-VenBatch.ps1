1a1ecc011541f779ddb73d57a69de38ddb8382b7
Greg Brownstein <greg.brownstein@venafi.com>
2024-09-12 19:31:03 -0400

    batching logging, confirmation

--- VenafiPS/Private/Select-VenBatch.ps1
+++ VenafiPS/Private/Select-VenBatch.ps1
@@ -1,18 +1,18 @@
 function Select-VenBatch {
     <#
     .SYNOPSIS
-    Batches pipeline input. 
+    Batches pipeline input.
 
     .DESCRIPTION
-    Batches up pipeline input into consistently sized List[T]s of objects. Used to ensure that processing occurs in specific sized batches. 
+    Batches up pipeline input into consistently sized List[T]s of objects. Used to ensure that processing occurs in specific sized batches.
     Useful for not recieving API timouts due to sending more objects than can be processed in the connection timeout period.
 
-    .PARAMETER InputObject 
+    .PARAMETER InputObject
     The pipeline input objects binds to this parameter one by one.
     Do not use it directly.
 
     .PARAMETER BatchSize
-    The size of the batches to separate the pipeline input into. 
+    The size of the batches to separate the pipeline input into.
 
     .PARAMETER BatchType
     Type of object to group things into. Defaults to a Powershell custom object
@@ -29,55 +29,58 @@ function Select-VenBatch {
 
     [CmdletBinding(PositionalBinding = $false)]
     param (
-      [Parameter(ValueFromPipeline)] 
-      $InputObject,
+        [Parameter(ValueFromPipeline)]
+        $InputObject,
 
-      [Parameter(Mandatory)]
-      [int] $BatchSize,
+        [Parameter(Mandatory)]
+        [int] $BatchSize,
 
-      [Parameter(Mandatory, Position = 0)]
-      [ValidateSet("pscustomobject","string","int","guid")]
-      [string] $BatchType = "pscustomobject"
+        [Parameter(Mandatory, Position = 0)]
+        [ValidateSet("pscustomobject", "string", "int", "guid")]
+        [string] $BatchType = "pscustomobject"
 
-      )
+    )
 
     Begin {
         switch ($BatchType) {
-            'string'         {
+            'string' {
                 $Batch = [System.Collections.Generic.Queue[string]]::new($BatchSize)
-                $OutList =  [System.Collections.Generic.List[string]]::new()
             }
-            'int'            {
+            'int' {
                 $Batch = [System.Collections.Generic.Queue[int]]::new($BatchSize)
-                $OutList =  [System.Collections.Generic.List[int]]::new()
             }
-            'guid'           {
+            'guid' {
                 $Batch = [System.Collections.Generic.Queue[guid]]::new($BatchSize)
-                $OutList =  [System.Collections.Generic.List[guid]]::new()
             }
             'pscustomobject' {
                 $Batch = [System.Collections.Generic.Queue[pscustomobject]]::new($BatchSize)
-                $OutList =  [System.Collections.Generic.List[pscustomobject]]::new()
             }
         }
+
+        $count = 0
     }
 
     Process {
+
+        $count++
+
         $Batch.Enqueue($_)
-        if ($Batch.Count -eq $BatchSize) { 
-            $OutList.AddRange($Batch)
-            ,$OutList
+
+        if ($Batch.Count -eq $BatchSize) {
+            'Processing batch {0}, items {1}-{2}' -f ($count / $BatchSize), ($count - $BatchSize + 1), $count | Write-Verbose
+            , ($Batch)
             $Batch.Clear() # start next batch
-            $OutList.clear()
         }
     }
 
     end {
-        if ($batch.Count) { #scriptblock geos here. 
-            $OutList.AddRange($Batch)
-            ,$OutList
-            $Batch.Clear() # start next batch
-            $OutList.clear()
+        # process any remaining items, eg. we didn't have a full batch
+        if ( $Batch.Count ) {
+            $batchNum = [math]::Ceiling($count / $BatchSize)
+            'Processing batch {0}, items {1}-{2}' -f $batchNum, ((($batchNum - 1) * $BatchSize) + 1), $count | Write-Verbose
+            , ($Batch)
+            $Batch.Clear()
+            Remove-Variable Batch
         }
     }
 }
