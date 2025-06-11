0282d1bbb29ba90f30f6165fb48de3a49a3f2108
Greg Brownstein <greg.brownstein@cyberark.com>
2025-06-11 19:22:59 -0400

    add vc app renewal config

--- VenafiPS/Public/Get-VcApplication.ps1
+++ VenafiPS/Public/Get-VcApplication.ps1
@@ -5,6 +5,7 @@ function Get-VcApplication {
 
     .DESCRIPTION
     Get 1 or more applications.
+    Application level renewal configurations are included.
 
     .PARAMETER Application
     Application ID or name
@@ -24,7 +25,11 @@ function Get-VcApplication {
     Get-VcApplication -Application 'ca7ff555-88d2-4bfc-9efa-2630ac44c1f2'
 
     applicationId              : 96fc9310-67ec-11eb-a8a7-794fe75a8e6f
-    certificateIssuingTemplate : @{Name=MyTemplate; id=7fb6af20-b22e-11ea-9a24-930fb5d2b247}
+    issuingTemplate            : @{Name=MyTemplate; id=7fb6af20-b22e-11ea-9a24-930fb5d2b247}
+    autoRenew                  : True
+    autoProvision              : True
+    renewalWindowInherit       : False
+    renewalWindowDays          : 15
     companyId                  : 09b24f81-b22b-11ea-91f3-ebd6dea5452e
     name                       : myapp
     description                :
@@ -101,19 +106,44 @@ function Get-VcApplication {
             }
         }
 
-        if ( $response.PSObject.Properties.Name -contains 'applications' ) {
-            $applications = $response | Select-Object -ExpandProperty applications
+        $applications = if ( $response.PSObject.Properties.Name -contains 'applications' ) {
+            $response | Select-Object -ExpandProperty applications
         }
         else {
-            $applications = $response
+            $response
         }
 
-        if ( $applications ) {
+        foreach ($app in $applications) {
+            $thisConfig = Invoke-VenafiRestMethod -UriLeaf ('autorenewal/{0}/configuration' -f $app.id)
             $applications | Select-Object @{'n' = 'applicationId'; 'e' = { $_.Id } },
             @{
                 'n' = 'issuingTemplate'
                 'e' = {
                     $_.certificateIssuingTemplateAliasIdMap.psobject.Properties | Select-Object @{'n' = 'name'; 'e' = { $_.Name } }, @{'n' = 'issuingTemplateId'; 'e' = { $_.Value } }
+                }
+            },
+            @{
+                'n' = 'autoRenew'
+                'e' = {
+                    $thisConfig.renewalActions.renew
+                }
+            },
+            @{
+                'n' = 'autoProvision'
+                'e' = {
+                    $thisConfig.renewalActions.provision
+                }
+            },
+            @{
+                'n' = 'renewalWindowInherit'
+                'e' = {
+                    $thisConfig.renewalWindow.inherit
+                }
+            },
+            @{
+                'n' = 'renewalWindowDays'
+                'e' = {
+                    $thisConfig.renewalWindow.days
                 }
             }, * -ExcludeProperty Id, certificateIssuingTemplateAliasIdMap
         }
